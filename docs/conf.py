3e60f062f8b1efdd1bf7f24c636ae3363daa9532
KageNoDev <154551168+KageNoDev@users.noreply.github.com>
2025-10-02 14:54:14 +0200

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@ project = 'MABAC Decision Support'
 author = 'Laura Białobrzewska'
 release = '0.1.0'
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
